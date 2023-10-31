4a812752835cf5c960b90d8305eca651bff4d7f0
kevinloeffler <hi@kevinloeffler.ch>
2023-10-31 20:08:44 +0100

    fix path to spacy model

--- scripts/train_spacy.py
+++ scripts/train_spacy.py
@@ -2,7 +2,7 @@ from lib.ner.models.spacy_model import SpacyModel
 
 
 def train_spacy(config):
-    base_model = 'models/ner/spacy_0'
+    base_model = config['paths']['models'] + 'ner/spacy_0'
     path_to_data = config['paths']['data'] + 'ner/manual_training_data/generated_historic/historic_data.csv'
     path_to_safe = config['paths']['models'] + 'ner/spacy'
     iterations = 1
