1021bcadd2fd6dea2e01286447fd68acdaa21e3a
kevinloeffler <hi@kevinloeffler.ch>
2023-10-31 20:33:37 +0100

    increase training iterations

--- scripts/train_spacy.py
+++ scripts/train_spacy.py
@@ -5,7 +5,7 @@ def train_spacy(config):
     base_model = config['paths']['models'] + 'ner/spacy_0'
     path_to_data = config['paths']['data'] + 'ner/manual_training_data/generated_historic/historic_data.csv'
     path_to_safe = config['paths']['models'] + 'ner/spacy'
-    iterations = 1
+    iterations = 200
 
     model = SpacyModel(base_model)
     model.train(iterations=iterations, with_training_csv=path_to_data, safe_to=path_to_safe)
