932f23bfa283d8de7da6a09af135514cd52f26d2
kevinloeffler <hi@kevinloeffler.ch>
2023-11-01 19:36:58 +0100

    add if name == main

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import tomli
 
-from scripts.train_spacy import train_spacy
+from scripts.evaluate_transformer import evaluate_transformer_model
 
-# config = None
+if __name__ == '__main__':
+    # config = None
+    with open('config.toml', mode='rb') as config_file:
+        config = tomli.load(config_file)
+        try:
+            config['paths']['data']
+        except KeyError:
+            print('ERROR: missing config key: paths.data')
+            quit(1)
+        try:
+            config['paths']['models']
+        except KeyError:
+            print('ERROR: missing config key: paths.models')
+            quit(1)
 
-with open('config.toml', mode='rb') as config_file:
-    config = tomli.load(config_file)
-    try:
-        config['paths']['data']
-    except KeyError:
-        print('ERROR: missing config key: paths.data')
-        quit(1)
-    try:
-        config['paths']['models']
-    except KeyError:
-        print('ERROR: missing config key: paths.models')
-        quit(1)
+    evaluate_transformer_model(config)
 
-
-train_spacy(config)
-
-
+# data = model.load_data('data/ner/manual_training_data/per_loc_1.csv')
+# print(data[200:250])
 
 # spacy_model = SpacyModel(model_name='de_core_news_sm')
 # test_fragment = load_data('manual_training_data/manual_training_data_1.csv')[345]
