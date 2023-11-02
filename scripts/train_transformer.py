2063117fb643b4b443caf90e3bf707a39130fc6c
kevinloeffler <hi@kevinloeffler.ch>
2023-11-03 00:16:45 +0100

    train bert_2

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -3,9 +3,9 @@ from lib.ner.models.transformer_model import TransformerModel
 
 def train_transformer_model(config):
     model = TransformerModel(model_type='bert',
-                             model_name='herokiller/german-bert-finetuned-ner',  # config['paths']['models'] + 'ner/transformers_2',
+                             model_name=config['paths']['models'] + 'ner/trf_bert_1',
                              numbers_of_gpus=config['number_of_gpus'],
                              training_iterations=200,
                              gpu_id=config['gpu_id'])
-    model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/per_loc_1.csv',  # 'ner/manual_training_data/generated_historic/historic_data.csv'
-                safe_to=config['paths']['models'] + 'ner/trf_bert_1')
+    model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/generated_historic/historic_data.csv',
+                safe_to=config['paths']['models'] + 'ner/trf_bert_2')
