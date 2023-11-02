93fa9c660fe348ac8ec84ecab4fbec9c1227c93b
kevinloeffler <hi@kevinloeffler.ch>
2023-11-02 16:51:08 +0100

    train off the shelf bert with 500 manual training data

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -2,11 +2,10 @@ from lib.ner.models.transformer_model import TransformerModel
 
 
 def train_transformer_model(config):
-    model = TransformerModel(model_type='roberta',
-                             model_name=config['paths']['models'] + 'ner/transformers_2',
+    model = TransformerModel(model_type='bert',
+                             model_name='herokiller/german-bert-finetuned-ner',  # config['paths']['models'] + 'ner/transformers_2',
                              numbers_of_gpus=config['number_of_gpus'],
                              training_iterations=200,
                              gpu_id=config['gpu_id'])
-    model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/generated_historic/historic_data.csv',
-                safe_to=config['paths']['models'] + 'ner/transformers')
-
+    model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/per_loc_1.csv',  # 'ner/manual_training_data/generated_historic/historic_data.csv'
+                safe_to=config['paths']['models'] + 'ner/trf_bert_1')
