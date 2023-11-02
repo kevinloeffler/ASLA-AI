9c25c95a510bf1ca96931fc18d44486e623ec0dd
kevinloeffler <hi@kevinloeffler.ch>
2023-11-02 14:50:28 +0100

    change model

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -3,8 +3,9 @@ from lib.ner.models.transformer_model import TransformerModel
 
 def train_transformer_model(config):
     model = TransformerModel(model_type='roberta',
-                             model_name='roberta-base',  # config['paths']['models'] + 'ner/transformers_2',
+                             model_name=config['paths']['models'] + 'ner/transformers_2',
                              numbers_of_gpus=config['number_of_gpus'],
-                             training_iterations=250)
+                             training_iterations=200)
     model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/generated_historic/historic_data.csv',
                 safe_to=config['paths']['models'] + 'ner/transformers')
+
