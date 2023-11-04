5564931b80a52a29584bfabde2fa8a5e0b948731
kevinloeffler <hi@kevinloeffler.ch>
2023-11-04 08:35:48 +0100

    train bert with manual and generated data

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -3,9 +3,9 @@ from lib.ner.models.transformer_model import TransformerModel
 
 def train_transformer_model(config):
     model = TransformerModel(model_type='bert',
-                             model_name=config['paths']['models'] + 'ner/trf_bert_1',
+                             model_name='bert-base-uncased',  # config['paths']['models'] + 'ner/trf_bert_1',
                              numbers_of_gpus=config['number_of_gpus'],
                              training_iterations=200,
                              gpu_id=config['gpu_id'])
     model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/generated_historic/historic_data.csv',
-                safe_to=config['paths']['models'] + 'ner/trf_bert_2')
+                safe_to=config['paths']['models'] + 'ner/trf_bert_3')
