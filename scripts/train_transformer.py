dfde14b97b711acd3141800a4ee6044b5de8475b
kevinloeffler <hi@kevinloeffler.ch>
2023-11-04 08:56:37 +0100

    train roberta model

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -2,10 +2,10 @@ from lib.ner.models.transformer_model import TransformerModel
 
 
 def train_transformer_model(config):
-    model = TransformerModel(model_type='bert',
-                             model_name='bert-base-uncased',  # config['paths']['models'] + 'ner/trf_bert_1',
+    model = TransformerModel(model_type='roberta',
+                             model_name='roberta-base',  # config['paths']['models'] + 'ner/trf_bert_1',
                              numbers_of_gpus=config['number_of_gpus'],
                              training_iterations=200,
                              gpu_id=config['gpu_id'])
     model.train(with_training_csv=config['paths']['data'] + 'ner/manual_training_data/combined_training.csv',
-                safe_to=config['paths']['models'] + 'ner/trf_bert_3')
+                safe_to=config['paths']['models'] + 'ner/trf_roberta_3')
