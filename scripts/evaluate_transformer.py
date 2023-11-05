3b4908bd2b60bd12cdcd9a8c86d4c1de4cf5e9f8
kevinloeffler <hi@kevinloeffler.ch>
2023-11-05 21:34:09 +0100

    evaluate trf_roberta_3

--- scripts/evaluate_transformer.py
+++ scripts/evaluate_transformer.py
@@ -3,14 +3,14 @@ from lib.ner.models.transformer_model import TransformerModel
 
 
 def evaluate_transformer_model(config):
-    model = TransformerModel(model_type='bert',
-                             model_name=config['paths']['models'] + 'ner/trf_bert_3',
+    model = TransformerModel(model_type='roberta',
+                             model_name=config['paths']['models'] + 'ner/trf_roberta_3',
                              numbers_of_gpus=config['number_of_gpus'],
                              training_iterations=1,
                              gpu_id=config['gpu_id'])
     performance = model.test(config['paths']['data'] + 'ner/manual_training_data/per_loc_validation_1.csv',
-                             output_file='model_evaluation/ner/trf_bert_3.txt')
-    safe_predictions_to_csv(to='model_evaluation/ner/trf_bert_3.csv', prediction_results=performance)
+                             output_file='model_evaluation/ner/trf_roberta_3.txt')
+    safe_predictions_to_csv(to='model_evaluation/ner/trf_roberta_3.csv', prediction_results=performance)
 
     # model = TransformerModel(model_type='roberta', model_name='roberta-base')
     # model = TransformerModel(model_type='bert', model_name='domischwimmbeck/bert-base-german-cased-fine-tuned-ner')
