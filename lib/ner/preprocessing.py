4332ce0f458aa8ea41bf5e11a36e727e93f39a21
kevinloeffler <hi@kevinloeffler.ch>
2023-11-09 09:05:05 +0100

    add combine nachlaesse

--- lib/ner/preprocessing.py
+++ lib/ner/preprocessing.py
@@ -1,4 +1,62 @@
 import csv
+import os
+import re
+
+
+def combine_nachlaesse(output_file: str):
+    entities = ['HEAD', 'MST', 'DATE']
+    data = {
+        'cramer-ernst': {
+            'path': 'nachlaesse/csv/Werkverzeichnis-Cramer-Ernst.csv', 'id': 'identifier', 'project': 'cre',
+            'labels': {'HEAD': 'plan_head', 'MST': 'note.scale', 'DATE': 'creation_date_start'}
+        },
+        'mertens-nussbaumer': {
+            'path': 'nachlaesse/csv/Werkverzeichnis-Mertens-Nussbaumer.csv', 'id': 'identifier', 'project': 'mnu',
+            'labels': {'HEAD': 'plan_head', 'MST': 'note.scale', 'DATE': 'creation_date_start'}
+        },
+        'klauser': {
+            'path': 'nachlaesse/csv/Werkverzeichnis-Klauser.csv', 'id': 'identifier', 'project': 'kla',
+            'labels': {'HEAD': 'plan_head', 'MST': 'scale', 'DATE': 'creation_date_start'}
+        },
+    }
+
+    with open(output_file, 'x') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(['project', 'id'] + entities)
+
+        for architect, info in data.items():
+            with open(info['path'], 'r') as file:
+                reader = csv.reader(file)
+                header = next(reader)
+                id_index = header.index(info['id'])
+                head_index = header.index(info['labels']['HEAD'])
+                mst_index = header.index(info['labels']['MST'])
+                date_index = header.index(info['labels']['DATE'])
+                for row in reader:
+                    writer.writerow([
+                        info['project'],
+                        row[id_index],
+                        row[head_index],
+                        format_mst(row[mst_index]),
+                        row[date_index],
+                    ])
+
+
+def format_mst(mst: str) -> str | None:
+    # Condition 1 - correct pattern: 1:100
+    if re.match(r'^\d+:\d+$', mst):
+        return mst
+
+    # Condition 2 - leading '1:': 1:1:100
+    if re.match(r'^\d+:\d+:\d+$', mst):
+        return re.sub(r'^\d+:', '', mst)
+
+    # Condition 3 - number only: 100
+    if re.match(r'^\d+$', mst):
+        return f'1:{mst}'
+
+    return None
+
 
 
 def extract_list_of_historic_municipalities(path_to_file: str):
