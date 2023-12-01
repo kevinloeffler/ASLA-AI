b587d37022340e8127c27e70360e9980a8f09459
kevinloeffler <hi@kevinloeffler.ch>
2023-12-01 14:07:24 +0100

    train spacy on new data

--- scripts/train_spacy.py
+++ scripts/train_spacy.py
@@ -5,7 +5,7 @@ def train_spacy(config):
     base_model = 'de_core_news_sm'  # config['paths']['models'] + 'ner/spacy_0'
     path_to_data = config['paths']['data'] + 'ner/training_set_250.csv'
     path_to_safe = config['paths']['models'] + 'ner/spacy'
-    iterations = 100
+    iterations = 50
 
     model = SpacyModel(base_model)
-    model.train(iterations=iterations, with_training_csv=path_to_data, safe_to=path_to_safe)
+    model.train(iterations=iterations, with_training_csv=path_to_data, safe_to=path_to_safe, gpu_id=config['gpu_id'])
