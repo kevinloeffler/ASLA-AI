b0e64e19122c16b2829b49654f2d5b562f8e8e45
kevinloeffler <hi@kevinloeffler.ch>
2023-12-12 15:29:53 +0100

    use local spacy base model

--- scripts/train_spacy.py
+++ scripts/train_spacy.py
@@ -2,7 +2,7 @@ from lib.ner.models.spacy_model import SpacyModel
 
 
 def train_spacy(config):
-    base_model = 'de_core_news_sm'  # config['paths']['models'] + 'ner/spacy_0'
+    base_model = config['paths']['models'] + 'ner/de_core_news_md'
     path_to_data = config['paths']['data'] + 'ner/training_set_200.csv'
     path_to_safe = config['paths']['models'] + 'ner/spacy'
     iterations = 50
