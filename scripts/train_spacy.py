625197181eff08bb0a67fcc20bedb57f9ba293ed
kevinloeffler <hi@kevinloeffler.ch>
2023-12-12 15:43:27 +0100

    train spacy with 100 it

--- scripts/train_spacy.py
+++ scripts/train_spacy.py
@@ -5,7 +5,7 @@ def train_spacy(config):
     base_model = config['paths']['models'] + 'ner/de_core_news_md/de_core_news_md-3.6.0'
     path_to_data = config['paths']['data'] + 'ner/training_set_200.csv'
     path_to_safe = config['paths']['models'] + 'ner/spacy'
-    iterations = 50
+    iterations = 100
 
     model = SpacyModel(base_model)
     model.train(iterations=iterations, with_training_csv=path_to_data, safe_to=path_to_safe, gpu_id=config['gpu_id'])
