7bb8ad37afe7b6ca5731f2e00871c01857151871
kevinloeffler <hi@kevinloeffler.ch>
2023-12-14 16:30:00 +0100

    train roberta-large-1

--- scripts/train_transformer.py
+++ scripts/train_transformer.py
@@ -3,10 +3,10 @@ from lib.ner.models.transformer_model import TransformerModel
 
 def train_transformer_model(config):
 
-    safe_to = config['paths']['models'] + 'ner/xlm-roberta-1'
+    safe_to = config['paths']['models'] + 'ner/roberta-large-1'
 
-    model = TransformerModel(model_type='xlm-roberta',
-                             model_name='xlm-roberta-base',  # config['paths']['models'] + 'ner/trf_bert_1',
+    model = TransformerModel(model_type='roberta',
+                             model_name='roberta-large',  # config['paths']['models'] + 'ner/trf_bert_1',
                              training_iterations=70,
                              safe_to=safe_to,
                              numbers_of_gpus=config['number_of_gpus'],
